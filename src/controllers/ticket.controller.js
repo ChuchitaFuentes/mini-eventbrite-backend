15143fe71339cd34deacea446dde6cfdceaa3f8f
ChuchitaFuentes <fuentes.nunez.maria.prog@gmail.com>
2025-08-28 18:01:31 -0600

    Feat: Se agregan endpoints faltantes para tickets

--- src/controllers/ticket.controller.js
+++ src/controllers/ticket.controller.js
@@ -5,7 +5,7 @@ import crypto from 'crypto';
 import { env } from '../config/env.js';
 
 
-export async function name(req, res, next) {
+export async function purchase(req, res, next) {
     try {
         const ticket = await Tickets.purchase(req.body, req.user.sub);
         res.status(201).json({ ticket });
